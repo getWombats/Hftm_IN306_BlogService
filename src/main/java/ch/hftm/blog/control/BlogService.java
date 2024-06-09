2866a286020fe44d6533c926f512bba79f0ad5be
Andres Soria <andres.soria@hftm.ch>
2024-06-09 21:46:48 +0200

    Updated: BlogService class

--- src/main/java/ch/hftm/blog/control/BlogService.java
+++ src/main/java/ch/hftm/blog/control/BlogService.java
@@ -1,24 +1,41 @@
 package ch.hftm.blog.control;
 
 import java.util.List;
+import java.util.Optional;
 
 import ch.hftm.blog.entity.Blog;
 import ch.hftm.blog.repository.BlogRepository;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.logging.Log;
-
-import jakarta.enterprise.context.Dependent;
+import io.quarkus.panache.common.Page;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
-@Dependent
+@ApplicationScoped
 public class BlogService {
     @Inject
     BlogRepository blogRepository;
 
     public List<Blog> getBlogs() {
-        var blogs = blogRepository.listAll();
+        return this.getBlogs(Optional.empty(), Optional.empty());
+    }
+
+    public List<Blog> getBlogs(Optional<String> searchString, Optional<Long> page) {
+        PanacheQuery<Blog> blogQuery;
+        if (searchString.isEmpty()) {
+            blogQuery = blogRepository.findAll();
+        } else {
+            blogQuery = blogRepository.find("title like ?1 or content like ?1", "%" + searchString.get() + "%");
+        }
+
+        List<Blog> blogs = blogQuery.page(Page.ofSize(20)).list();
         Log.info("Returning " + blogs.size() + " blogs");
         return blogs;
+    }
+
+    public Blog getBlogById(long id) {
+        return blogRepository.findById(id);
     }
 
     @Transactional
