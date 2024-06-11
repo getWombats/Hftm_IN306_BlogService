e4003f18a954ad8ff5594c7a0d60416b19a9c113
Andres Soria <andres.soria@hftm.ch>
2024-06-11 22:48:13 +0200

    Updated: Implemented http requests
    Requests implemented:
    - DELETE
    - PUT
    - PATCH

--- src/main/java/ch/hftm/blog/boundary/BlogRessource.java
+++ src/main/java/ch/hftm/blog/boundary/BlogRessource.java
@@ -6,9 +6,13 @@ import ch.hftm.blog.control.BlogService;
 import ch.hftm.blog.entity.Blog;
 import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
@@ -29,15 +33,77 @@ public class BlogRessource {
 
     @GET
     @Path("{id}")
-    public Response getBlog(long id) {
+    public Response getBlog(Long id) {
+
+        if(id == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
         return Response.status(Status.OK).entity(blogService.getBlogById(id)).build();
     }
 
     @POST
     public Response addBlog(Blog blog, @Context UriInfo uriInfo) {
+
         blogService.addBlog(blog);
+
         Log.info("POST Request: " + blog);
         URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(blog.getId())).build();
         return Response.created(uri).entity(blog).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteBlog(@PathParam("id") Long id) {
+
+        if(id == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        Blog deletedBlog = blogService.deleteBlog(id);
+
+        if (deletedBlog != null) {
+            return Response.status(Status.NO_CONTENT).entity(deletedBlog).build();
+        } else {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    public Response putBlog(@PathParam("id") Long id, Blog blog) {
+
+        if(blog == null || id == null) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        if (blog.getTitle().isEmpty() || blog.getContent().isEmpty()) {
+            return Response.status(Status.BAD_REQUEST).build();
+        } else {
+            Blog replacedBlog = blogService.replaceBlog(id, blog);
+
+            if (replacedBlog != null) {
+            return Response.status(Status.OK).entity(replacedBlog).build();
+            } else {
+                return Response.status(Status.NOT_FOUND).build();
+            }
+        }
+    }
+
+    @PATCH
+    @Path("{id}")
+    public Response patchBlog(@PathParam("id") Long id, Blog blog) {
+        
+        if(blog == null || id == null || id != blog.getId()) {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        
+        Blog updatedBlog = blogService.updateBlog(id, blog);
+
+        if (updatedBlog != null) {
+            return Response.status(Status.OK).entity(updatedBlog).build();
+        } else {
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
 }
