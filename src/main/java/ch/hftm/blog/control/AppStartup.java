2a7d26518b325761cb7de615900b125db22908cf
Andres Soria <andres.soria@hftm.ch>
2024-06-11 22:46:28 +0200

    Created: New class for testing purpose
    Adds on application startup some blog objects for testing

---
+++ src/main/java/ch/hftm/blog/control/AppStartup.java
@@ -0,0 +1,24 @@
+package ch.hftm.blog.control;
+
+import ch.hftm.blog.entity.Blog;
+import io.quarkus.runtime.Startup;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+@Startup 
+public class AppStartup {
+    
+    @Inject
+    BlogService blogService;
+
+    @PostConstruct
+    public void init() {
+        blogService.addBlog(new Blog(null, "Ich und mein Quarkus", "Quarkus ist ein Framework, das auf Java basiert und für die Entwicklung von Cloud-nativen Anwendungen optimiert ist."));
+        blogService.addBlog(new Blog(null, "Was sind GET Requests?", "GET ist eine Methode, die von einem Client verwendet wird, um Daten von einem Server abzurufen."));
+        blogService.addBlog(new Blog(null, "Was ist ein POST Request?", "POST ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden."));
+        blogService.addBlog(new Blog(null, "Was ist ein PUT Request?", "PUT ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu ersetzen."));
+        blogService.addBlog(new Blog(null, "Was ist ein PATCH Request?", "PATCH ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu aktualisieren."));
+    }
+}
