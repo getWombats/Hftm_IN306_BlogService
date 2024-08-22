b9df5dbd984eecf76badae9ecb8081eac6b8589f
Andres Soria <andres.soria@hftm.ch>
2024-08-23 00:49:56 +0200

    Update: Refactored BlogRessource

--- src/main/java/ch/hftm/blog/boundary/BlogRessource.java
+++ src/main/java/ch/hftm/blog/boundary/BlogRessource.java
@@ -1,12 +1,19 @@
 package ch.hftm.blog.boundary;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
 import ch.hftm.blog.control.BlogService;
-import ch.hftm.blog.model.entity.Blog;
-import io.quarkus.logging.Log;
+import ch.hftm.blog.control.CommentService;
+import ch.hftm.blog.model.dto.BlogDTO;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HeaderParam;
@@ -14,96 +21,129 @@ import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriInfo;
 
+// @DenyAll
 @Path("blogs")
 public class BlogRessource {
 
     @Inject
     BlogService blogService;
 
+    @Inject
+    CommentService commentService;
+
+    @Inject
+    JsonWebToken jwt;
+
     @GET
-    public Response getBlogs(@QueryParam("searchString") Optional<String> searchString,
-            @QueryParam("page") Optional<Long> page) {
-        return Response.status(Status.OK).entity(blogService.getBlogs(searchString, page)).build();
+    @PermitAll
+    public Response getBlogs(@QueryParam("searchString") Optional<String> searchString, @QueryParam("page") Optional<Long> page) {
+        List<BlogDTO> blogDtos = blogService.getBlogs(searchString, page);
+
+        if(blogDtos == null || blogDtos.isEmpty()) {
+            return Response.status(Status.NOT_FOUND).entity("No blogs found.").build();
+        }
+
+        return Response.status(Status.OK).entity(blogDtos).build();
     }
 
     @GET
     @Path("{id}")
-    public Response getBlog(Long id) {
+    @PermitAll
+    public Response getBlog(Long blogId) {
 
-        if(id == null) {
-            return Response.status(Status.BAD_REQUEST).build();
+        if(blogId == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog-id provided.").build();
         }
 
-        return Response.status(Status.OK).entity(blogService.getBlogById(id)).build();
+        BlogDTO foundBlog = blogService.getBlogById(blogId);
+
+        if(foundBlog == null) {
+            return Response.status(Status.NOT_FOUND).entity("No blog with id " + blogId + "available.").build();
+        }
+
+        return Response.status(Status.OK).entity(foundBlog).build();
     }
 
     @POST
-    public Response addBlog(Blog blog, @Context UriInfo uriInfo) {
+    public Response addBlog(@Valid BlogDTO blog, @Context UriInfo uriInfo) {
 
-        blogService.addBlog(blog);
+        if(blog == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog provided.").build();
+        }
 
-        Log.info("POST Request: " + blog);
-        URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(blog.getId())).build();
-        return Response.created(uri).entity(blog).build();
+        String username = jwt.getClaim(Claims.preferred_username.name());
+        blog.setAuthorName(username);
+
+        BlogDTO addedBlog = blogService.addBlog(blog);
+
+        if(addedBlog == null) {
+            return Response.status(Status.NOT_MODIFIED).entity("Error while adding blog.").build();
+        }
+
+        URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(addedBlog.getId())).build();
+        return Response.created(uri).entity(addedBlog).build();
     }
 
     @DELETE
-    public Response deleteBlog(@HeaderParam("id") Long id) {
+    public Response deleteBlog(@HeaderParam("blogId") Long blogId) {
 
-        if(id == null) {
-            return Response.status(Status.BAD_REQUEST).build();
+        if(blogId == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog-id provided.").build();
         }
 
-        Blog deletedBlog = blogService.deleteBlog(id);
+        BlogDTO deletedBlog = blogService.deleteBlog(blogId);
 
-        if (deletedBlog != null) {
-            return Response.status(Status.NO_CONTENT).entity(deletedBlog).build();
-        } else {
-            return Response.status(Status.NOT_FOUND).build();
+        if (deletedBlog == null) {
+            return Response.status(Status.NOT_FOUND).entity("Action not performed. Blog with id " + blogId + " was not found.").build();
         }
+
+        return Response.status(Status.NO_CONTENT).entity(deletedBlog).build();
     }
 
     @PUT
-    @Path("{id}")
-    public Response putBlog(@PathParam("id") Long id, Blog blog) {
+    public Response putBlog(@HeaderParam("blogId") Long blogId,@Valid BlogDTO blog) {
 
-        if(blog == null || id == null) {
-            return Response.status(Status.BAD_REQUEST).build();
+        if(blogId == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog-id provided.").build();
         }
 
-        if (blog.getTitle().isEmpty() || blog.getContent().isEmpty()) {
-            return Response.status(Status.BAD_REQUEST).build();
-        } else {
-            Blog replacedBlog = blogService.replaceBlog(id, blog);
-
-            if (replacedBlog != null) {
-            return Response.status(Status.OK).entity(replacedBlog).build();
-            } else {
-                return Response.status(Status.NOT_FOUND).build();
-            }
+        if(blog == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog provided.").build();
         }
+
+        BlogDTO replacedBlog = blogService.replaceBlog(blogId, blog);
+
+        if (replacedBlog != null) {
+            return Response.status(Status.NOT_FOUND).build();
+        
+        }
+
+        return Response.status(Status.OK).entity(replacedBlog).build();
     }
 
     @PATCH
-    public Response patchBlog(@HeaderParam("id") Long id, Blog blog) {
+    public Response patchBlog(@HeaderParam("blogId") Long blogId,@Valid BlogDTO blog) {
         
-        if(blog == null || id == null) {
-            return Response.status(Status.BAD_REQUEST).build();
+        if(blogId == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog-id provided.").build();
         }
-        
-        Blog updatedBlog = blogService.updateBlog(id, blog);
 
-        if (updatedBlog != null) {
-            return Response.status(Status.OK).entity(updatedBlog).build();
-        } else {
-            return Response.status(Status.NOT_FOUND).build();
+        if(blog == null) {
+            return Response.status(Status.BAD_REQUEST).entity("No blog provided.").build();
         }
+        
+        BlogDTO updatedBlog = blogService.updateBlog(blogId, blog);
+
+        if (updatedBlog == null) {
+            return Response.status(Status.NOT_FOUND).entity("Action not performed. Blog with id " + blogId + " was not found.").build();
+        }
+
+        return Response.status(Status.OK).entity(updatedBlog).build();
     }
 }
