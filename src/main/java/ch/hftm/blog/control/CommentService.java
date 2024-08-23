63db9108a7d1eb59c6e6289a243ba3fd3919cb8c
Andres Soria <andres.soria@hftm.ch>
2024-08-23 13:04:44 +0200

    Update: CommentService
    Changed methods to take dto objects and also return dto objects

--- src/main/java/ch/hftm/blog/control/CommentService.java
+++ src/main/java/ch/hftm/blog/control/CommentService.java
@@ -1,11 +1,15 @@
 package ch.hftm.blog.control;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
+import ch.hftm.blog.model.dto.CommentDTO;
 import ch.hftm.blog.model.entity.Blog;
 import ch.hftm.blog.model.entity.Comment;
 import ch.hftm.blog.repository.BlogRepository;
 import ch.hftm.blog.repository.CommentRepository;
+import ch.hftm.blog.service.DtoConverter;
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -18,6 +22,9 @@ public class CommentService {
 
     @Inject
     BlogRepository blogRepository;
+
+    @Inject
+    DtoConverter dtoConverter;
 
     public List<Comment> getAllComments() {
         return commentRepository.listAll();
@@ -39,21 +46,30 @@ public class CommentService {
     }
 
     @Transactional
-    public Comment addComment(Comment comment, Long blogId) {
+    public Comment addComment(CommentDTO comment, Long blogId) {
 
         comment.setCreatedAt(LocalDateTime.now());
 
-        commentRepository.persist(comment);
+        Comment commentToAdd = dtoConverter.fromCommentDto(comment);
+        commentRepository.persist(commentToAdd);
+
+        Comment addedComment = commentRepository.find("content = ?1 and createdAt = ?2", comment.getContent(),
+                comment.getCreatedAt()).firstResult();
+
+        if(addedComment == null) {
+            Log.warn("Comment for blog with id " + blogId + " was not persisted.");
+            return null;
+        }
 
         Blog blog = blogRepository.findById(blogId);
-        blog.getComments().add(comment);
+        blog.getComments().add(commentToAdd);
         blogRepository.persist(blog);
 
-        return comment;
+        return addedComment;
     }
 
     @Transactional
-    public Comment deleteComment(Long commentId) {
+    public CommentDTO deleteComment(Long commentId) {
 
         Comment comment = commentRepository.findById(commentId);
 
@@ -61,20 +77,20 @@ public class CommentService {
             commentRepository.delete(comment);
         }
 
-        return comment;
+        return dtoConverter.toCommentDto(comment);
     }
 
     @Transactional
-    public Comment updateComment(Comment updatedComment) {
+    public CommentDTO updateComment(CommentDTO updatedComment) {
 
         Comment existingComment = commentRepository.findById(updatedComment.getId());
 
         if (existingComment != null) {
             existingComment.setContent(updatedComment.getContent());
-            existingComment.setLastEditedAt(LocalDateTime.now());
+            existingComment.setLastEditedAt(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
             commentRepository.persist(existingComment);
         }
 
-        return existingComment;
+        return dtoConverter.toCommentDto(existingComment);
     }
 }
