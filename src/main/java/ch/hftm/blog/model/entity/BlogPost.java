cf9fd8fe773244c1f1991a97d71f709c8a983df2
Andres Soria <andres.soria@hftm.ch>
2024-08-28 21:22:41 +0200

    Update: Added new object reference and properties

--- src/main/java/ch/hftm/blog/model/entity/BlogPost.java
+++ src/main/java/ch/hftm/blog/model/entity/BlogPost.java
@@ -3,7 +3,10 @@ package ch.hftm.blog.model.entity;
 import java.time.Instant;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,13 +22,14 @@ import lombok.NoArgsConstructor;
 public class BlogPost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "blog_id")
     private Long id;
     private String title;
     private String content;
-    private String authorName;
+    private String author;
     private Instant createdAt;
     private Instant lastEditedAt;
 
-    @OneToMany
+    @OneToMany(mappedBy = "blog", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Comment> comments;
 }
--- src/main/java/ch/hftm/blog/model/entity/Comment.java
+++ src/main/java/ch/hftm/blog/model/entity/Comment.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -15,13 +17,16 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String authorName;
+    private int commentNumber;
     private String content;
+    private String author;
     private Instant createdAt;
     private Instant lastEditedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    private BlogPost blog;
 }
