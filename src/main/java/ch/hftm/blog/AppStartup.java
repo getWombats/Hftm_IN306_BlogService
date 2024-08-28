673be38b6d98024f17ef116faa1da2e17b8e32b9
Andres Soria <andres.soria@hftm.ch>
2024-08-28 21:34:33 +0200

    Update: Refactored appstartup
    Initialize with blog objects for better testing other apps

--- src/main/java/ch/hftm/blog/AppStartup.java
+++ src/main/java/ch/hftm/blog/AppStartup.java
@@ -1,25 +1,95 @@
 package ch.hftm.blog;
 
-import ch.hftm.blog.control.BlogService;
-import ch.hftm.blog.model.entity.Blog;
+import ch.hftm.blog.control.BlogPostService;
+import ch.hftm.blog.control.CommentService;
+import ch.hftm.blog.model.dto.BlogPostDTO;
+import ch.hftm.blog.model.dto.CommentDTO;
 import io.quarkus.runtime.Startup;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
-@Startup 
+@Startup
 public class AppStartup {
-    
-    @Inject
-    BlogService blogService;
+        @Inject
+        BlogPostService blogService;
 
-    @PostConstruct
-    public void init() {
-        // blogService.addBlog(new Blog(null, "Ich und mein Quarkus", "Quarkus ist ein Framework, das auf Java basiert und für die Entwicklung von Cloud-nativen Anwendungen optimiert ist."));
-        // blogService.addBlog(new Blog(null, "Was sind GET Requests?", "GET ist eine Methode, die von einem Client verwendet wird, um Daten von einem Server abzurufen."));
-        // blogService.addBlog(new Blog(null, "Was ist ein POST Request?", "POST ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden."));
-        // blogService.addBlog(new Blog(null, "Was ist ein PUT Request?", "PUT ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu ersetzen."));
-        // blogService.addBlog(new Blog(null, "Was ist ein PATCH Request?", "PATCH ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu aktualisieren."));
-    }
+        @Inject
+        CommentService commentService;
+
+        @PostConstruct
+        public void init() {
+                // initializeDummyBlogs();
+                // initializeDummyComments();
+        }
+
+        private void initializeDummyBlogs() {
+                blogService.addBlogPost(
+                                new BlogPostDTO(null,
+                                                "Ich und mein Quarkus",
+                                                "Quarkus ist ein Framework, das auf Java basiert und für die Entwicklung von Cloud-nativen Anwendungen optimiert ist.",
+                                                "Chuck Norris",
+                                                null,
+                                                null,
+                                                null));
+
+                blogService.addBlogPost(
+                                new BlogPostDTO(null,
+                                                "Was sind GET Requests?",
+                                                "GET ist eine Methode, die von einem Client verwendet wird, um Daten von einem Server abzurufen.",
+                                                "Chuck Norris",
+                                                null,
+                                                null,
+                                                null));
+
+                blogService.addBlogPost(
+                                new BlogPostDTO(null,
+                                                "Was ist ein POST Request?",
+                                                "POST ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden.",
+                                                "Chuck Norris",
+                                                null,
+                                                null,
+                                                null));
+
+                blogService.addBlogPost(
+                                new BlogPostDTO(null,
+                                                "Was ist ein PUT Request?",
+                                                "PUT ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu ersetzen.",
+                                                "Chuck Norris",
+                                                null,
+                                                null,
+                                                null));
+
+                blogService.addBlogPost(
+                                new BlogPostDTO(null,
+                                                "Was ist ein PATCH Request?",
+                                                "PATCH ist eine Methode, die von einem Client verwendet wird, um Daten an einen Server zu senden und zu aktualisieren.",
+                                                "Chuck Norris",
+                                                null,
+                                                null,
+                                                null));
+        }
+
+        private void initializeDummyComments() {
+                commentService.addCommentToBlog(1,
+                                new CommentDTO(
+                                                null,
+                                                null,
+                                                0,
+                                                "Ja Quarkus ist schon ganz gut aber ab und zu verwirrend, vorallem mit hibernate orm...",
+                                                "Chuck Norris",
+                                                null,
+                                                null));
+
+                commentService.addCommentToBlog(4,
+                                new CommentDTO(
+                                                null,
+                                                null,
+                                                0,
+                                                "PUT Requests sind irgendwie nutzlos. Eine Mischung zwischen POST und PATCH... das Einsatzgebiet dafür erscheint mir nicht ganz plausibel.",
+                                                "Chuck Norris",
+                                                null,
+                                                null));
+        }
 }
