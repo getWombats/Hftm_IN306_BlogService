1e8c79f79fbcb5157bf0cb134eb671131972a078
Andres Soria <andres.soria@hftm.ch>
2024-08-28 21:37:11 +0200

    Create: New abstract class for inheritance

---
+++ src/main/java/ch/hftm/blog/boundary/ResourceBase.java
@@ -0,0 +1,6 @@
+package ch.hftm.blog.boundary;
+
+public abstract class ResourceBase {
+    protected final String MISSING_BLOG_ID_IN_PATH_MESSAGE = "Missing Blog-ID in path.";
+    protected final String MISSING_COMMENT_ID_IN_PATH_MESSAGE = "Missing Comment-ID in path.";
+}
